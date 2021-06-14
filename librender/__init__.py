d9e5f90ee965172ac1ad4be2dddebc76cb483b9f
Jingnan Shi <jnshi@mit.edu>
2021-06-14 17:12:19 -0400

    add folders to python path

--- librender/__init__.py
+++ librender/__init__.py
@@ -1,6 +1,7 @@
 import ctypes
-import os
+import os, sys
 
 pyrender_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, pyrender_dir)
 ctypes.cdll.LoadLibrary(os.path.join(pyrender_dir, 'pyrender.so'))
 from pyrender import *
